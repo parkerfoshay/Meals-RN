4e70eab430de079983bd38cec7d4805cc9cc8862
Parker Faucher <parkerfaucher@Parkers-MacBook-Pro.local>
2019-10-10 13:22:45 -0700

    started tab navigation along with the favorites screen. added header button to favorite certain meals.

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { StyleSheet, Text, View, Button } from "react-native";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
 
 import { MEALS } from "../data/dummy-data";
+import HeaderButton from "../components/HeaderButton";
 
 const MealDetailScreen = ({ navigation: { popToTop, getParam } }) => {
-  const mealId = getParam('mealId');
+  const mealId = getParam("mealId");
 
-  const selectedMeal = MEALS.find(meal => meal.id === mealId)
+  const selectedMeal = MEALS.find(meal => meal.id === mealId);
   return (
     <View style={styles.screen}>
       <Text>{selectedMeal.title}</Text>
@@ -27,7 +29,12 @@ MealDetailScreen.navigationOptions = navigationData => {
   const selectedMeal = MEALS.find(meal => meal.id === mealId);
 
   return {
-    headerTitle: selectedMeal.title
+    headerTitle: selectedMeal.title,
+    headerRight: (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item title="Favorite" iconName="ios-star" onPress={() => {}} />
+      </HeaderButtons>
+    )
   };
 };
 
